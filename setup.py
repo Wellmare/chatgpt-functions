62a5c3c4e282e5c8a91f3c425b254bb01e5ce276
Ivan <ivan.kolipov@gmail.com>
2023-06-28 11:18:11 +0300

    Add read_requirements in setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,17 @@ def readme():
         return f.read()
 
 
+def read_requirements():
+    with open('requirements.txt', 'rb') as file:
+        content = file.read().decode('utf-8')
+    lines = content.splitlines()
+    lines = [line.strip() for line in lines if line.strip()]
+    return lines
+
+
 setup(
     name="chatgpt_functions",
-    version="0.1.1",
+    version="0.1.4",
     author="Wellmare",
     author_email="ivan.kolipov@gmail.com",
     description="Wrapper over the gpt3.5 model, capable of calling functions",
@@ -16,7 +24,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Wellmare/chatgpt-functions",
     packages=find_packages(),
-    install_requires=["openai==0.27.8", "python-dotenv==1.0.0"],
+    install_requires=read_requirements(),
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
